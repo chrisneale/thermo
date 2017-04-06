0d08e9d44372d095ceed231d7ab51b65fdf1677c
Chris Neale <christopher.neale@gmail.com>
2017-04-06 16:17:38 +0000

    Fixed typo

--- routes/temp-raw.js
+++ routes/temp-raw.js
@@ -3,7 +3,12 @@ var router = express.Router();
 var temp = require('temperature');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(temp.temperature);
+  var data = {
+  	"datetime" : new Date(),
+	"temperature" : temp.temperature
+  };
+  res.send(data);
+
 });
 
 module.exports = router;
